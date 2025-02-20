1ca6810c0a308171c665b37488b1f0996cd3e547
PabloTey <pablotey8@gmail.com>
2025-02-21 00:10:47 +0100

    implementacion listado ejercicios

--- FitQuestPrototype/src/main/java/com/salesianos/FitQuestPrototype/Entrenamiento/Services/EjercicioService.java
+++ FitQuestPrototype/src/main/java/com/salesianos/FitQuestPrototype/Entrenamiento/Services/EjercicioService.java
@@ -6,11 +6,17 @@ import com.salesianos.FitQuestPrototype.Entrenamiento.Repos.EjercicioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class EjercicioService {
 
     private final EjercicioRepository ejercicioRepository;
+
+    public List<Ejercicio> findAllEjercicio(){
+        return ejercicioRepository.findAllEjercicios();
+    }
 
     public Ejercicio save (CreateEjercicioCmd createEjercicioCmd){
         return ejercicioRepository.save(Ejercicio.builder()
