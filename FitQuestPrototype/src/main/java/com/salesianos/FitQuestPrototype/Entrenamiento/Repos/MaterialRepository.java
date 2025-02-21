e98d7b629a9151f57cb67b09b64b846f29733841
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2025-02-21 13:53:39 +0100

    Implementacion find all material

--- FitQuestPrototype/src/main/java/com/salesianos/FitQuestPrototype/Entrenamiento/Repos/MaterialRepository.java
+++ FitQuestPrototype/src/main/java/com/salesianos/FitQuestPrototype/Entrenamiento/Repos/MaterialRepository.java
@@ -2,6 +2,15 @@ package com.salesianos.FitQuestPrototype.Entrenamiento.Repos;
 
 import com.salesianos.FitQuestPrototype.Entrenamiento.Model.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+
+    @Query("""
+    SELECT m
+    FROM Material m
+""")
+    List<Material> FindAllMateriales();
 }
