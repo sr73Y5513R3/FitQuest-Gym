54c02511713b08ef6be51b14b7e14730607fdf53
PabloTey <pablotey8@gmail.com>
2025-06-15 16:29:30 +0200

    avances parte admin

--- FitQuesWeb/src/app/Shared/menu-lateral/menu-lateral.component.ts
+++ FitQuesWeb/src/app/Shared/menu-lateral/menu-lateral.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { AuthService } from '../../Services/login/auth-service.service';
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu-lateral',
@@ -9,19 +11,28 @@ import { AuthService } from '../../Services/login/auth-service.service';
 })
 export class MenuLateralComponent {
   
+  isDropdownOpen: boolean = false;
   isAdmin$: Observable<boolean>;
   isLoggedIn$: Observable<boolean>; // Opcional: para mostrar/ocultar elementos basados en si el usuario está logeado
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.isAdmin$ = this.authService.isAdmin();
-    this.isLoggedIn$ = this.authService.isAuthenticated$; // Puedes usar isAuthenticated$ para esto
+    this.isLoggedIn$ = this.authService.isAuthenticated$;
   }
 
   ngOnInit(): void {
-    // Si necesitas alguna lógica de inicialización aquí
+     if(this.router.url.startsWith('/admin/')){
+      this.isDropdownOpen = true;
+    };
   }
 
   logout(): void {
     this.authService.logout();
   }
+
+  toggleAdminPanel(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  
 }
